3245e5c97eac24b1f2b743e8e911d52e5adde412
LeXa <lexxaa@seznam.cz>
2018-09-16 18:04:00 +0200

    USART structure used as pointer in defines

--- 01_Drivers/usart/usart.h
+++ 01_Drivers/usart/usart.h
@@ -2,7 +2,7 @@
  * usart.h
  *
  * Created: 11.10.2014 21:07:33
- * Revised: 19.4.2017
+ * Revised: 16.9.2018
  * Author: LeXa
  * BOARD: 
  *
@@ -19,28 +19,28 @@
 /************************************************************************/
 /* DEFINES                                                              */
 /************************************************************************/
-#define usart_config(usart_ch,ChSize,Parity)                usart_ch.CTRLC = ChSize|Parity|USART_CMODE_ASYNCHRONOUS_gc|0<<USART_SBMODE_bp
-#define usart_tx_enable(usart_ch)                           usart_ch.CTRLB |= USART_TXEN_bm
-#define usart_rx_enable(usart_ch)                           usart_ch.CTRLB |= USART_RXEN_bm
-#define usart_tx_disable(usart_ch)                          usart_ch.CTRLB &= ~USART_TXEN_bm
-#define usart_rx_disable(usart_ch)                          usart_ch.CTRLB &= ~USART_RXEN_bm
+#define usart_config(usart_ch,ChSize,Parity)                usart_ch->CTRLC = ChSize|Parity|USART_CMODE_ASYNCHRONOUS_gc|0<<USART_SBMODE_bp
+#define usart_tx_enable(usart_ch)                           usart_ch->CTRLB |= USART_TXEN_bm
+#define usart_rx_enable(usart_ch)                           usart_ch->CTRLB |= USART_RXEN_bm
+#define usart_tx_disable(usart_ch)                          usart_ch->CTRLB &= ~USART_TXEN_bm
+#define usart_rx_disable(usart_ch)                          usart_ch->CTRLB &= ~USART_RXEN_bm
                                                             
-#define usart_set_interrupt(usart_ch,tx_int,rx_int,dre_int) usart_ch.CTRLA = rx_int|tx_int|dre_int;
+#define usart_set_interrupt(usart_ch,tx_int,rx_int,dre_int) usart_ch->CTRLA = rx_int|tx_int|dre_int;
 
-#define usart_baud(usart_ch,Fcpu,Baud)                      usart_ch.BAUDCTRLA = (uint16_t) (Fcpu/16/Baud-1) & 0x00FF;\
-                                                            usart_ch.BAUDCTRLB = (uint16_t) (((Fcpu/16/Baud-1) & 0x0F00)>>8)
+#define usart_baud(usart_ch,Fcpu,Baud)                      usart_ch->BAUDCTRLA = (uint16_t) (Fcpu/16/Baud-1) & 0x00FF;\
+                                                            usart_ch->BAUDCTRLB = (uint16_t) (((Fcpu/16/Baud-1) & 0x0F00)>>8)
 
-#define usart_port_config(port_ch,unTxPin,unRxPin)          port_ch.OUTSET = 1<<unTxPin;\
-                                                            port_ch.DIRSET = 1<<unTxPin;\
-                                                            port_ch.DIRCLR = 1<<unRxPin
+#define usart_port_config(port_ch,unTxPin,unRxPin)          port_ch->OUTSET = 1<<unTxPin;\
+                                                            port_ch->DIRSET = 1<<unTxPin;\
+                                                            port_ch->DIRCLR = 1<<unRxPin
 
-#define usart_send(usart_ch,byte)                           usart_ch.DATA = byte;\
-                                                            while (!(usart_ch.STATUS & USART_TXCIF_bm));\
-                                                            usart_ch.STATUS |= USART_TXCIF_bm
+#define usart_send(usart_ch,byte)                           usart_ch->DATA = byte;\
+                                                            while (!(usart_ch->STATUS & USART_TXCIF_bm));\
+                                                            usart_ch->STATUS |= USART_TXCIF_bm
 
-#define usart_read(usart_ch,byte)                           while(!(usart_ch.STATUS & USART_RXCIF_bm));\
-                                                            usart_ch.STATUS |= USART_RXCIF_bm;\
-                                                            byte = usart_ch.DATA
+#define usart_read(usart_ch,byte)                           while(!(usart_ch->STATUS & USART_RXCIF_bm));\
+                                                            usart_ch->STATUS |= USART_RXCIF_bm;\
+                                                            byte = usart_ch->DATA
 
 
 /************************************************************************/
